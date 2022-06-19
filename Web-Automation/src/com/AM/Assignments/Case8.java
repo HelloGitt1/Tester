28cefdd1d09fa3c40954f16487b95b3b44cc467b
HelloGitt1 <lokeshrsai@gmail.com>
2022-06-19 13:56:39 +0530

    Initial Draft

---
+++ Web-Automation/src/com/AM/Assignments/Case8.java
@@ -0,0 +1,119 @@
+package com.AM.Assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Case8 {
+
+	public static WebDriver oBrowser=null;
+	public static void main(String[] args) {
+		launchBrowser();
+		navigate();
+		login();
+		minimizeFlyOutWindow();
+		Importtask();
+		logout();
+		closeApplication();
+	}
+	private static void launchBrowser()
+	{
+		try
+		{
+			System.setProperty("webdriver.chrome.driver", "D:\\SeleniumAutomation\\POI\\Web-Automation\\Library\\drivers\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+			oBrowser.manage().window().maximize();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void navigate()
+	{
+		try
+		{
+			oBrowser.get("http://localhost:82/login.do");
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void closeApplication()
+	{
+		try
+		{
+			oBrowser.quit();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void login()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("admin");
+			oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void minimizeFlyOutWindow()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void logout()
+	{
+		try
+		{
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void Importtask()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//div[text()='TASKS']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//div[text()='Add New Task']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//div[text()='Import tasks from CSV']")).click();
+			Thread.sleep(2000);
+			
+			
+			
+			oBrowser.findElement(By.xpath("//div[text()='Complete Import']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\"closeLoadTasksFromCSVPopupButton\"]")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
---
+++ Web-Automation/src/com/sgtesting/tests/FireFoxBrowserDemo.java
@@ -0,0 +1,45 @@
+package com.sgtesting.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxBrowserDemo {
+	public static WebDriver oBrowser=null;
+	public static void main(String[] args) {
+		LaunchBrowser();
+		navigate();
+		closeApplication();
+	}
+	private static void LaunchBrowser()
+	{
+		try
+		{
+			System.setProperty("webdriver.gecko.driver","D:\\SeleniumAutomation\\POI\\Web-Automation\\Library\\drivers\\geckodriver.exe");
+			oBrowser=new FirefoxDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	private static void navigate()
+	{
+		try
+		{
+			oBrowser.get("http://localhost:82/login.do");
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	private static void closeApplication()
+	{
+		try
+		{
+			oBrowser.close();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
